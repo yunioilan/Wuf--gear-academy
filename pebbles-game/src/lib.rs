37a5428d6245a14b18f29266f45ae6c125721174
yunioilan <giers888@gmail.com>
2024-07-24 21:05:03 +0800

    Update lib.rs

--- pebbles-game/src/lib.rs
+++ pebbles-game/src/lib.rs
@@ -7,16 +7,16 @@ static mut PEBBLES_GAME:Option<GameState> = None;
 
 #[no_mangle]
  extern "C" fn init() {
-
+    //
 }
 
 #[no_mangle]
  extern "C" fn handle() {
-
+   //
 }
 
 #[no_mangle]
  extern "C" fn state() {
-
+       //
 }
 
